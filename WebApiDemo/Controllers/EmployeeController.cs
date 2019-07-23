22123b9556f295184898e6ba80e9e2a66f4596f2
Zeeshan Siddique <zeeshansiddique49@gmail.com>
2019-07-23 13:34:06 +0500

    Add project files.

---
+++ WebApiDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IHttpActionResult GetAllEmployees()
+        {
+            IList<EmployeeViewModel> Employees = null;
+
+            using (var ctx = new EmployeeEntities())
+            {
+                Employees = ctx.employees.Include("EmployeeAddress")
+                            .Select(e => new EmployeeViewModel()
+                            {
+                                Id = e.ID,
+                                FirstName = e.FirstName,
+                                LastName = e.LastName
+                            }).ToList<EmployeeViewModel>();
+            }
+
+            if (Employees.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(Employees);
+        }
+    }
+}
